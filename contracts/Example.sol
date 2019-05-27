c20dca7d705b4367730f53c59e03063721e2d297
cristoforo <cristoforo.mione@gmail.com>
2019-05-27 12:38:35 +0200

    dummy string variable added to contract

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -2,11 +2,16 @@ pragma solidity ^0.5.0;
 
 contract Example {
   uint public value;
+  string public name;
   string test = "test";
 
   function setValue(uint256 _value) public {
     // test test
     value = _value;
+  }
+
+  function setName(string memory _name) public {
+    name = _name;
   }
 
   function testBoom() internal pure {
--- test/example.test.js
+++ test/example.test.js
@@ -8,21 +8,44 @@ contract('Example', async accounts => {
     ;[owner] = accounts
   })
 
-  it('gets default value', async () => {
-    const contract = await Example.deployed()
-    const value = await contract.value.call()
-    assert.equal(value.toNumber(), 0, "0 wasn't the default value")
+  describe('value', () => {
+    it('gets default value', async () => {
+      const contract = await Example.deployed()
+      const value = await contract.value.call()
+      assert.equal(value.toNumber(), 0, "0 wasn't the default value")
+    })
+
+    it('sets a value', async () => {
+      const contract = await Example.deployed()
+      const mockValue = 5
+      await contract.setValue(mockValue, { from: owner })
+      const value = await contract.value.call()
+      assert.equal(
+        value.toNumber(),
+        mockValue,
+        `${mockValue} wasn't the return value instead got ${value}`
+      )
+    })
   })
 
-  it('sets a value', async () => {
-    const contract = await Example.deployed()
-    const mockValue = 5
-    await contract.setValue(mockValue, { from: owner })
-    const value = await contract.value.call()
-    assert.equal(
-      value.toNumber(),
-      mockValue,
-      `${mockValue} wasn't the return value instead got ${value}`
-    )
+  describe('name', () => {
+    it('gets default name', async () => {
+      const contract = await Example.deployed()
+      const name = await contract.name.call()
+      assert.equal(name, '', "(empty) wasn't the default name")
+      assert.equal(name.length, 0)
+    })
+
+    it('sets a name', async () => {
+      const contract = await Example.deployed()
+      const mockName = 'OtherName'
+      await contract.setName(mockName, { from: owner })
+      const name = await contract.name.call()
+      assert.equal(
+        name,
+        mockName,
+        `${mockName} wasn't the return value instead got ${name}`
+      )
+    })
   })
 })
